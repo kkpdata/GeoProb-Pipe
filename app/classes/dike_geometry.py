1c3867b3d2fde378090501ee0e28ef6436f20d2b
Oscar Ophof <o.ophof@wshd.com>
2024-10-16 14:06:20 +0200

    nieuwe sterkteberekeningen py toegevoegd

--- app/classes/dike_geometry.py
+++ app/classes/dike_geometry.py
@@ -5,25 +5,6 @@ import geopandas as gpd
 import numpy as np
 import pandas as pd
 
-sys.path.append(str(Path(__file__).parents[1]))  # Add repo to sys.path to make sure all imports are correctly found
-
-# tijdelijk dataframe voor testen, kolommen komen overeen met input vanuit sheet
-df_dike_geometry = pd.read_excel(
-    r"V:\dr_Waterkeringen\08. Kennis\02. Probabilitische rekenen - werkmap\GeoProb-Pipe\testcase 20-4 STPH\Test_bestand_geoprob_pipe.xlsx",
-    sheet_name="test_vak_par",
-)
-
-df_traject_par = pd.read_excel(
-    r"V:\dr_Waterkeringen\08. Kennis\02. Probabilitische rekenen - werkmap\GeoProb-Pipe\testcase 20-4 STPH\Test_bestand_geoprob_pipe.xlsx",
-    sheet_name="test_traject_par",
-)
-
-df_general_par = pd.read_excel(
-    r"V:\dr_Waterkeringen\08. Kennis\02. Probabilitische rekenen - werkmap\GeoProb-Pipe\testcase 20-4 STPH\Test_bestand_geoprob_pipe.xlsx",
-    sheet_name="test_gen_par",
-)
-
-
 class DikeGeometry:
 
     def __init__(self, gdf_dike_geometry):  #: gpd.GeoDataFrame
@@ -117,8 +98,3 @@ class DikeGeometry:
         self.df_deklagen["D effectieve deklaag [m]"] = d_effectief
 
         return self.df_deklagen
-
-
-check_script = DikeGeometry(df_dike_geometry)
-
-display(check_script.effectivieve_deklaag)
