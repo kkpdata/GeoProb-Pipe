df24d85d75893e63e8462dae80fc8c99e9fb25c1
chrispijo <chrispitzalis@gmail.com>
2025-05-20 11:13:12 +0200

    coverage trial

--- app/classes/test_vak.py
+++ app/classes/test_vak.py
@@ -10,15 +10,15 @@ def test_vak_collection():
 
     assert 1+1 == 2
 
-    repo_root = repository_root_path()
-    workspace_path = os.path.join(repo_root, "workspaces", "example_new_calculations")
-    workspace = Workspace(workspace_path)
-    df_overview_parameters = read_excel(
-        workspace.input.folderpath / "input.xlsx", sheet_name="Overzicht_parameters", index_col=0, header=0).rename(
-        columns=lambda x: x.strip())
-    df_vakken = read_excel(workspace.input.folderpath / "input.xlsx", sheet_name="Vakken").rename(
-        columns=lambda x: x.strip())
-    # noinspection PyUnusedLocal
-    vak_collection = VakCollection(df_vakken, df_overview_parameters)
+    # repo_root = repository_root_path()
+    # workspace_path = os.path.join(repo_root, "workspaces", "example_new_calculations")
+    # workspace = Workspace(workspace_path)
+    # df_overview_parameters = read_excel(
+    #     workspace.input.folderpath / "input.xlsx", sheet_name="Overzicht_parameters", index_col=0, header=0).rename(
+    #     columns=lambda x: x.strip())
+    # df_vakken = read_excel(workspace.input.folderpath / "input.xlsx", sheet_name="Vakken").rename(
+    #     columns=lambda x: x.strip())
+    # # noinspection PyUnusedLocal
+    # vak_collection = VakCollection(df_vakken, df_overview_parameters)
 
     ##
