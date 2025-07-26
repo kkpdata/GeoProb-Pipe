fc90d0e903859f25ce8f5ce4e5aeaa04d80f6872
chrispijo <chrispitzalis@gmail.com>
2025-07-26 08:59:22 +0200

    Prevent import

--- repo_utils/collect_todos.py
+++ repo_utils/collect_todos.py
@@ -1,7 +1,33 @@
 import os
-from geoprob_pipe.utils.other import repository_root_path
 from typing import Tuple
 from pandas import DataFrame
+from typing import Optional
+import os
+from git import Repo, InvalidGitRepositoryError
+from typing import Optional
+
+
+def repository_root_path() -> Optional[str]:
+
+    # Bold guess, it is the execution path
+    base_dir = os.getcwd()
+    try:
+        repo_root = Repo(base_dir, search_parent_directories=False).working_tree_dir
+        return repo_root
+    except InvalidGitRepositoryError:
+        pass
+
+    # Otherwise, search subdirectories
+    for subdir, dirs, files in os.walk(os.getcwd()):
+        for directory in dirs:
+            try:
+                repo = Repo(os.path.join(subdir, directory), search_parent_directories=False)
+                return repo.working_tree_dir
+            except InvalidGitRepositoryError:
+                continue
+        return None
+    return None
+
 
 
 def get_todo_contents(line: str) -> Tuple[bool, str, str, str, str]:
