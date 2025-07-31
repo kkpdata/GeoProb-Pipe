41d3373f9a0a1a7a68d2f57b396d4c0b721b8cae
Laura van der Doef <lvanderdoef@outlook.com>
2025-07-31 16:31:02 +0200

    verbeterd

--- geoprob_pipe/visualizations/graphs/hfreq.py
+++ geoprob_pipe/visualizations/graphs/hfreq.py
@@ -8,17 +8,16 @@ if TYPE_CHECKING:
 
 def export_hfreq_graphs(geoprob_pipe: GeoProbPipe):
     """Grafiek van de overschrijdingsfrequentielijn van de waterstand per HydraNL uitvoerpunt"""
-    gevonden_locaties = []
 
     export_dir = os.path.join(geoprob_pipe.visualizations.graphs.export_dir, "grafiek_hfreq")
     os.makedirs(export_dir, exist_ok=True)
 
-    df_uitredepunten = geoprob_pipe.input_data.uittredepunten.df
+    df_uittredepunten = geoprob_pipe.input_data.uittredepunten.df
     for hydra_nl_name in geoprob_pipe.input_data.overschrijdingsfrequentielijnen.keys():
         hfreq = geoprob_pipe.input_data.overschrijdingsfrequentielijnen[hydra_nl_name]
         levels = hfreq.overschrijdingsfrequentielijn.level
         freq =  hfreq.overschrijdingsfrequentielijn.exceedance_frequency
-        uittredepunten = list(df_uitredepunten[df_uitredepunten['hydra_locatie_id'] == hydra_nl_name]['uittredepunt_id'])
+        uittredepunten = list(df_uittredepunten[df_uittredepunten['hydra_locatie_id'] == hydra_nl_name]['uittredepunt_id'])
         plt.ioff()
         plt.figure(figsize=(8, 5))
         plt.plot(levels, freq, marker='o', linestyle='-', color='blue',markersize=1)
@@ -31,28 +30,3 @@ def export_hfreq_graphs(geoprob_pipe: GeoProbPipe):
         plt.tight_layout()
         export_path = os.path.join(export_dir, f"{hydra_nl_name}_hfreq.png")
         plt.savefig(export_path)
-
-
-
-
-##% Oude code Ard-Jan
-# print("\nFragility Curve (CDF):")
-#     print(f"{'x':>20} {'P(failure)':>25}")
-#     print("-" * 50)
-#     for fc in sorted(project.variables["WS"].fragility_values, key=lambda f: f.x):
-#         print(f"{fc.x:20} {fc.probability_of_failure:25}")
-
-#     fragility_vals = sorted(project.variables["WS"].fragility_values, key=lambda f: f.x)
-#     x_vals = [fc.x for fc in fragility_vals]
-#     p_vals = [max(fc.probability_of_failure, 1e-10) for fc in fragility_vals]  # voorkom log(0)
-
-#     plt.figure(figsize=(8, 5))
-#     plt.plot(x_vals, p_vals, marker='o', linestyle='-', color='blue',markersize=1)
-#     plt.xscale('linear')  # belasting vaak lineair
-#     plt.yscale('log')     # faalkans logaritmisch
-#     plt.xlabel("Belasting (x)")
-#     plt.ylabel("Kans op falen (log-schaal)")
-#     plt.title("Fragility Curve (CDF)")
-#     plt.grid(True, which="both", linestyle='--', linewidth=0.5)
-#     plt.tight_layout()
-#     plt.show()
