296a67991b286425879a23119c77733be84ef08e
Vincent Jilesen <v.jilesen@wshd.nl>
2025-12-09 10:23:33 +0100

    limit state dumbbell plots

--- geoprob_pipe/comparisons/beta_dumbbell.py
+++ geoprob_pipe/comparisons/beta_dumbbell.py
@@ -2,7 +2,7 @@ import plotly.graph_objects as go
 from geoprob_pipe.comparisons.result_collect import ComparisonCollecter
 
 
-def dumbbell_uittredepunt(comparison: ComparisonCollecter,
+def dumbbell_beta(comparison: ComparisonCollecter,
                           export: bool = False):
     df_result1 = (comparison.df1_beta_uittredepunten[
         ["uittredepunt_id", "beta"]].rename(columns={"beta": "beta1"}))
@@ -12,6 +12,16 @@ def dumbbell_uittredepunt(comparison: ComparisonCollecter,
     df = df_result1.merge(df_result2, on="uittredepunt_id")
 
     fig = go.Figure()
+    for _, row in df.iterrows():
+        fig.add_trace(go.Scatter(
+            x=[row["uittredepunt_id"], row["uittredepunt_id"]],
+            y=[row["beta1"], row["beta2"]],
+            mode="lines",
+            showlegend=False,
+            marker=dict(
+                color="grey"
+            )
+        ))
 
     fig.add_trace(go.Scatter(
         x=df["uittredepunt_id"],
@@ -23,6 +33,7 @@ def dumbbell_uittredepunt(comparison: ComparisonCollecter,
                 size=10
             )
     ))
+
     fig.add_trace(go.Scatter(
         x=df["uittredepunt_id"],
         y=df["beta2"],
@@ -33,6 +44,39 @@ def dumbbell_uittredepunt(comparison: ComparisonCollecter,
                 size=10
             )
     ))
+
+    fig.update_layout(
+        title="Dumbbell Plot: Uittredepunt Beta Comparison",
+        xaxis_title="Uittredepunt ID",
+        yaxis_title="β-value",
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        )
+    )
+    fig.show()
+    return
+
+
+def dumbbell_uplift(comparison: ComparisonCollecter,
+                          export: bool = False):
+    df_result1 = (comparison.df1_beta_limit_states[
+        ["uittredepunt_id", "limit_state", "beta"]]
+                  .rename(columns={"beta": "beta1",
+                                   "limit_state": "limit_state1"}))
+    df_result1 = df_result1[df_result1["limit_state1"] == "calc_Z_u"]
+    df_result2 = (comparison.df2_beta_limit_states[
+        ["uittredepunt_id", "limit_state", "beta"]]
+                  .rename(columns={"beta": "beta2",
+                                   "limit_state": "limit_state2"}))
+    df_result2 = df_result2[df_result2["limit_state2"] == "calc_Z_u"]
+
+    df = df_result1.merge(df_result2, on="uittredepunt_id")
+
+    fig = go.Figure()
     for _, row in df.iterrows():
         fig.add_trace(go.Scatter(
             x=[row["uittredepunt_id"], row["uittredepunt_id"]],
@@ -43,10 +87,168 @@ def dumbbell_uittredepunt(comparison: ComparisonCollecter,
                 color="grey"
             )
         ))
+
+    fig.add_trace(go.Scatter(
+        x=df["uittredepunt_id"],
+        y=df["beta1"],
+        mode="markers",
+        name=comparison.name_1,
+        marker=dict(
+                color="green",
+                size=10
+            )
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=df["uittredepunt_id"],
+        y=df["beta2"],
+        mode="markers",
+        name=comparison.name_2,
+        marker=dict(
+                color="blue",
+                size=10
+            )
+    ))
+
     fig.update_layout(
-        title="Dumbbell Plot: Uittredepunt Beta Comparison",
+        title="Dumbbell Plot: Uittredepunt Calc_Z_u Beta Comparison",
         xaxis_title="Uittredepunt ID",
-        yaxis_title="β-value"
+        yaxis_title="β-value",
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        )
+    )
+    fig.show()
+    return
+
+def dumbbell_heave(comparison: ComparisonCollecter,
+                          export: bool = False):
+    df_result1 = (comparison.df1_beta_limit_states[
+        ["uittredepunt_id", "limit_state", "beta"]]
+                  .rename(columns={"beta": "beta1",
+                                   "limit_state": "limit_state1"}))
+    df_result1 = df_result1[df_result1["limit_state1"] == "calc_Z_h"]
+    df_result2 = (comparison.df2_beta_limit_states[
+        ["uittredepunt_id", "limit_state", "beta"]]
+                  .rename(columns={"beta": "beta2",
+                                   "limit_state": "limit_state2"}))
+    df_result2 = df_result2[df_result2["limit_state2"] == "calc_Z_h"]
+
+    df = df_result1.merge(df_result2, on="uittredepunt_id")
+
+    fig = go.Figure()
+    for _, row in df.iterrows():
+        fig.add_trace(go.Scatter(
+            x=[row["uittredepunt_id"], row["uittredepunt_id"]],
+            y=[row["beta1"], row["beta2"]],
+            mode="lines",
+            showlegend=False,
+            marker=dict(
+                color="grey"
+            )
+        ))
+
+    fig.add_trace(go.Scatter(
+        x=df["uittredepunt_id"],
+        y=df["beta1"],
+        mode="markers",
+        name=comparison.name_1,
+        marker=dict(
+                color="green",
+                size=10
+            )
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=df["uittredepunt_id"],
+        y=df["beta2"],
+        mode="markers",
+        name=comparison.name_2,
+        marker=dict(
+                color="blue",
+                size=10
+            )
+    ))
+
+    fig.update_layout(
+        title="Dumbbell Plot: Uittredepunt Calc_Z_h Beta Comparison",
+        xaxis_title="Uittredepunt ID",
+        yaxis_title="β-value",
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        )
+    )
+    fig.show()
+    return
+
+def dumbbell_piping(comparison: ComparisonCollecter,
+                          export: bool = False):
+    df_result1 = (comparison.df1_beta_limit_states[
+        ["uittredepunt_id", "limit_state", "beta"]]
+                  .rename(columns={"beta": "beta1",
+                                   "limit_state": "limit_state1"}))
+    df_result1 = df_result1[df_result1["limit_state1"] == "calc_Z_p"]
+    df_result2 = (comparison.df2_beta_limit_states[
+        ["uittredepunt_id", "limit_state", "beta"]]
+                  .rename(columns={"beta": "beta2",
+                                   "limit_state": "limit_state2"}))
+    df_result2 = df_result2[df_result2["limit_state2"] == "calc_Z_p"]
+
+    df = df_result1.merge(df_result2, on="uittredepunt_id")
+
+    fig = go.Figure()
+    for _, row in df.iterrows():
+        fig.add_trace(go.Scatter(
+            x=[row["uittredepunt_id"], row["uittredepunt_id"]],
+            y=[row["beta1"], row["beta2"]],
+            mode="lines",
+            showlegend=False,
+            marker=dict(
+                color="grey"
+            )
+        ))
+
+    fig.add_trace(go.Scatter(
+        x=df["uittredepunt_id"],
+        y=df["beta1"],
+        mode="markers",
+        name=comparison.name_1,
+        marker=dict(
+                color="green",
+                size=10
+            )
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=df["uittredepunt_id"],
+        y=df["beta2"],
+        mode="markers",
+        name=comparison.name_2,
+        marker=dict(
+                color="blue",
+                size=10
+            )
+    ))
+
+    fig.update_layout(
+        title="Dumbbell Plot: Uittredepunt Calc_Z_p Beta Comparison",
+        xaxis_title="Uittredepunt ID",
+        yaxis_title="β-value",
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        )
     )
     fig.show()
     return
--- main_comparison_vsc.py
+++ main_comparison_vsc.py
@@ -1,8 +1,11 @@
 from geoprob_pipe.comparisons.result_collect import ComparisonCollecter
-from geoprob_pipe.comparisons.beta_dumbbell import dumbbell_uittredepunt
+import geoprob_pipe.comparisons.beta_dumbbell as beta_dumbbell
 
 filepath1 = r"tests\systeem_testen\224\Traject224_model4a_WBN_prob.geoprob_pipe.gpkg"
 filepath2 = r"tests\systeem_testen\224\Traject224_MORIA_WBN_prob.geoprob_pipe.gpkg"
 comparison = ComparisonCollecter(filepath1, filepath2)
 
-dumbbell_uittredepunt(comparison)
+beta_dumbbell.dumbbell_beta(comparison)
+beta_dumbbell.dumbbell_uplift(comparison)
+beta_dumbbell.dumbbell_heave(comparison)
+beta_dumbbell.dumbbell_piping(comparison)
