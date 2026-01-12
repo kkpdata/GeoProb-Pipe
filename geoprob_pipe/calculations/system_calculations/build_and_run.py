99506614147b9a9f0efd8d609acd9be7f512fd87
Vincent Jilesen <v.jilesen@wshd.nl>
2026-01-12 14:29:47 +0100

    turbo aangezet

--- geoprob_pipe/calculations/system_calculations/build_and_run.py
+++ geoprob_pipe/calculations/system_calculations/build_and_run.py
@@ -16,36 +16,32 @@ if TYPE_CHECKING:
     from geoprob_pipe import GeoProbPipe
     from geoprob_pipe.calculations.system_calculations.system_base_objects.base_system_build import BaseSystemBuilder
 
+_BUILDER = None
+_MODEL = None
 
-def _worker(row_unique,
-            geohydrologisch_model: str,
-            geopackage_filepath: str,
-            to_run_vakken_ids: list[int]
-            ):
-    try:
-        system_builder: BaseSystemBuilder = (
-            SYSTEM_CALCULATION_MAPPER[geohydrologisch_model]['system_builder'](
-                geopackage_filepath=geopackage_filepath,
-                to_run_vakken_ids=to_run_vakken_ids)
-            )
 
-        calc = system_builder.build_instance(row_unique=row_unique)
-        calc.run()
+def _init_worker(geohydrologisch_model, geopackage_filepath, to_run_vakken_ids):
+    global _BUILDER, _MODEL
+    _MODEL = geohydrologisch_model
+    _BUILDER = (
+        SYSTEM_CALCULATION_MAPPER[geohydrologisch_model]["system_builder"](
+            geopackage_filepath=geopackage_filepath,
+            to_run_vakken_ids=to_run_vakken_ids,
+            ))
 
-        df_limit_state = collect_df_beta_per_limit_state(calc)
-        df_scenario = collect_df_beta_per_scenario(calc)
-        df_stochast = collect_stochast_values(calc)
-        df_derived = calculate_derived_values(df_scenario,
-                                              geohydrologisch_model)
-        df_scenario = df_scenario.drop(columns=["system_calculation"])
-    except Exception as e:
-        print(f"[PID {os.getpid()}] ERROR: {e}", flush=True)
-        raise
+
+def _worker(row_unique: dict):
+    calc = _BUILDER.build_instance(row_unique=row_unique)
+    calc.run()
+
+    df_limit_state = collect_df_beta_per_limit_state(calc)
+    df_scenario = collect_df_beta_per_scenario(calc)
+    df_stochast = collect_stochast_values(calc)
+    df_derived = calculate_derived_values(df_scenario,
+                                          _MODEL)
+    df_scenario = df_scenario.drop(columns=["system_calculation"])
+
     return df_limit_state, df_scenario, df_stochast, df_derived, calc.validation_messages
-
-
-def _starify(args):
-    return _worker(*args)
 
 
 def build_and_run_system_calculations(geoprob_pipe: GeoProbPipe):
@@ -64,25 +60,31 @@ def build_and_run_system_calculations(geoprob_pipe: GeoProbPipe):
     logger.info("Now running calculations...")
     n_threads: int = cpu_count() - 1
     n_calc_totaal: int = len(df_unique_combos)
-    chunk_size: int = math.ceil(n_calc_totaal / (n_threads * 10))
-    logger.info(f"Running with chunks of {chunk_size} calculations with {n_threads} parallel threads.")
+    chunk_size: int = max(math.ceil(n_calc_totaal / (n_threads * 10)), 5)
+    logger.info(
+        f"Running {n_calc_totaal} calculations in chunks of {chunk_size}" +
+        f" with {n_threads} parallel threads.")
     logger.info(f"Progress: 0 / {n_calc_totaal} calculations.")
 
-    args = [(row, geohydrologisch_model,
-             geopackage_filepath, to_run_vakken_ids) for _, row in
-            df_unique_combos.iterrows()]
+    rows = [
+        dict(zip(df_unique_combos.columns, r))
+        for r in df_unique_combos.itertuples(index=False, name=None)
+        ]
 
     last_report = time.time()
     done = 0
     results = []
-    pool_size = (min(math.floor(n_calc_totaal / chunk_size), n_threads)
-                 if n_calc_totaal > 0 else 1)
-    with Pool(processes=pool_size) as pool:
-        for res in pool.imap_unordered(_starify, args, chunksize=chunk_size):
+    pool_size = max(min(math.floor(n_calc_totaal / chunk_size), n_threads), 1)
+
+    with Pool(processes=pool_size, initializer=_init_worker, initargs=(
+        geohydrologisch_model, geopackage_filepath, to_run_vakken_ids
+        )) as pool:
+        for res in pool.imap_unordered(_worker, rows, chunksize=chunk_size):
             results.append(res)
             done += 1
-            now = time.time()
-            if now - last_report >= 30:
-                logger.info(f"Progress: {done} / {n_calc_totaal} calculations.")
-                last_report = now
+            if done % chunk_size == 0:
+                now = time.time()
+                if now - last_report >= 30:
+                    logger.info(f"Progress: {done} / {n_calc_totaal} calculations.")
+                    last_report = now
     return results
--- main_piping_vsc.py
+++ main_piping_vsc.py
@@ -7,10 +7,10 @@ from geoprob_pipe.questionnaire.cmd import ApplicationSettings
 
 if __name__ == "__main__":
     app_settings = ApplicationSettings()
-    app_settings.to_run = "vakken:10,11,12,13"
+    # app_settings.to_run = "vakken:10,11,12,13,14,15,16,17"
     filepath = r"C:\Users\vinji\Python\GEOprob-Pipe\Bestandenuitwisseling\Analyse16-1_V5.geoprob_pipe\Analyse16-1_V5.geoprob_pipe.gpkg"
     app_settings.workspace_dir = os.path.dirname(filepath)
     app_settings.geopackage_filename = os.path.basename(filepath)
 
     geoprob_pipe = GeoProbPipe(app_settings)
-    geoprob_pipe.export_archive()
+    # geoprob_pipe.export_archive()
