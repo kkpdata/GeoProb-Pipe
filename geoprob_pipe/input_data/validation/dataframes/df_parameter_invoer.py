4b9ab36ecebede86257cbce96749238a4c5998a7
chrispijo <chrispitzalis@gmail.com>
2026-02-17 16:39:01 +0100

    Tussenstand

--- geoprob_pipe/input_data/validation/dataframes/df_parameter_invoer.py
+++ geoprob_pipe/input_data/validation/dataframes/df_parameter_invoer.py
@@ -18,4 +18,11 @@ FAILURE_QUERIES: List[FailureQuery] = [
         msg="Geen variatie coefficient of standaard deviatie gespecificeerd voor deze rij. Dit is vereist bij "
             "distributie types 'normal' en 'log_normal'."
     ),
+    FailureQuery(
+        query="parameter in ['d70', 'd70_m'] "
+              "and distribution_type in ['deterministic', 'log_normal', 'normal'] "
+              "and mean < 0.001",
+        msg="Parameter d70/d70_m moet een waarde hebben die kleiner is dan 0.001. Deze geef je op in de eenheid "
+            "meters. Wellicht is de eenheid van de opgegeven waarde verkeerd?"
+    ),
 ]
