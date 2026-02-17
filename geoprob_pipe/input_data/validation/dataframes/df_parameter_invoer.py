c55a7fbb47afec86e538028ed5df0545c7e91ed2
chrispijo <chrispitzalis@gmail.com>
2026-02-17 15:18:32 +0100

    First concept of query version implemented

--- geoprob_pipe/input_data/validation/dataframes/df_parameter_invoer.py
+++ geoprob_pipe/input_data/validation/dataframes/df_parameter_invoer.py
@@ -1,5 +1,4 @@
 from typing import List
-# from pandera.pandas import DataFrameSchema, Column, Check
 from geoprob_pipe.input_data.validation.dataframes.validation_objects import FailureQuery
 
 
@@ -20,41 +19,3 @@ FAILURE_QUERIES: List[FailureQuery] = [
             "distributie types 'normal' en 'log_normal'."
     ),
 ]
-
-
-# DISTRIBUTION_TYPE_CDF_CURVE = DataFrameValidation(
-#     label="df_parameter_invoer",
-#     label_humanized="Parameter invoer",
-#     schemas=[
-#         ValidationSchema(
-#             label_humanized="Validatie voor distributie type 'cdf_curve'",
-#             query="distribution_type == 'cdf_curve'",
-#             schema=DataFrameSchema(columns={"fragility_values_ref": Column(str)},
-#         )),
-#         ValidationSchema(
-#             label_humanized="Validatie voor distributie type 'normal' en 'log_normal'",
-#             query="distribution_type == 'normal' or distribution_type == 'log_normal'",
-#             schema=DataFrameSchema(
-#                 columns={
-#                     "variation": Column(float, nullable=True, checks=Check(lambda s: s > 10)),
-#                     "deviation": Column(float, nullable=True),
-#                 },
-#                 checks=[
-#                     Check(lambda df: df["variation"].notna() | df["deviation"].notna(),
-#                           name="variation_or_deviation_provided"),
-#                 ],
-#         )),
-#     ]
-# )
-#
-#
-# DF_PARAMETER_INVOER_VALIDATION: List[DataFrameValidation] = [
-#     DISTRIBUTION_TYPE_CDF_CURVE,
-# ]
-#
-#
-# obj = DataFrameSchema(
-#     columns={
-#         "variation": Column(float, nullable=True, checks=Check(lambda item: item > 10)),
-#     },
-# )
