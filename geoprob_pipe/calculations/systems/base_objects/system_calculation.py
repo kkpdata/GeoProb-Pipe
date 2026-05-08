b21b3709fdd9b47fea0258b198c93223870cd964
chrispijo <chrispitzalis@gmail.com>
2026-05-08 14:53:14 +0200

    Pydra-core vast gezet. Anders error

--- geoprob_pipe/calculations/systems/base_objects/system_calculation.py
+++ geoprob_pipe/calculations/systems/base_objects/system_calculation.py
@@ -16,6 +16,15 @@ DEFAULT_RELIABILITY_SETTINGS: Dict[str, Union[str, float, int]] = {
     "relaxation_factor": 0.4,
 }
 
+# DEFAULT_RELIABILITY_SETTINGS: Dict[str, Union[str, float, int]] = {
+#     "reliability_method": ReliabilityMethod.form.__str__(),
+#     "variation_coefficient": 0.02,
+#     "maximum_iterations": 10_000,
+#     "relaxation_factor": 0.4,
+#     "epsilon_beta": 0.05,
+#     "relaxation_loops": 10,
+# }
+
 
 class SystemSetup:
 
