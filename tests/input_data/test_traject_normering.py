adf114bbd5eb224bee72c5cd5df6dcfa648ae323
chrispijo <chrispitzalis@gmail.com>
2026-05-21 11:57:07 +0200

    In progress: Vraag aan Sander

---
+++ tests/input_data/test_traject_normering.py
@@ -0,0 +1,19 @@
+
+
+def test_class_traject_normering():
+    ##
+
+    from geoprob_pipe.input_data.traject_normering import TrajectNormering
+    from repo_utils.utils import repository_root_path
+    import os
+    from geoprob_pipe.cmd_app.cmd import ApplicationSettings
+
+    app_settings = ApplicationSettings()
+    repo_root = repository_root_path()
+    filepath = os.path.join(repo_root, "tests", "systeem_testen", "224", "Traject224_MORIA_WBN_prob.geoprob_pipe.gpkg")
+    app_settings.workspace_dir = os.path.dirname(filepath)
+    app_settings.geopackage_filename = os.path.basename(filepath)
+
+    obj = TrajectNormering(app_settings=app_settings)
+
+    ##
