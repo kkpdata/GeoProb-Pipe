a7d021d74361fcc02f5f46d20e99bb48bef019ad
chrispijo <chrispitzalis@gmail.com>
2026-05-29 10:11:00 +0200

    Assertion on geometry types from vakindeling projection

--- geoprob_pipe/cmd_app/spatial_layers/vakindeling.py
+++ geoprob_pipe/cmd_app/spatial_layers/vakindeling.py
@@ -148,11 +148,10 @@ def validate_vakindeling(app_settings: ApplicationSettings, gdf: GeoDataFrame):
     # Validate geometry types
     allowed_types = {"LineString", "MultiLineString"}
     valid, invalid_types = validate_geometry_types(gdf=gdf, allowed_types=allowed_types)
-    if not valid:
-        raise ValueError(f"De opgegeven vakindeling heeft geometrie types die niet toegestaan zijn. "
-                         f"De volgende types zijn toegestaan: {allowed_types}. "
-                         f"De volgende niet toegestane types werden ook gevonden: {invalid_types}."
-                         f"Zorg er voor dat je alleen line strings importeert.")
+    assert valid, (f"De opgegeven vakindeling heeft geometrie types die niet toegestaan zijn. De volgende types zijn "
+                   f"toegestaan: {allowed_types}. De volgende niet toegestane types werden ook gevonden: "
+                   f"{invalid_types}. "
+                   f"Zorg er voor dat je alleen line strings importeert.")
 
     # Convergeer multi line strings naar single line strings
     gdf = convert_mls_geom_column_to_ls(gdf=gdf)
@@ -161,6 +160,8 @@ def validate_vakindeling(app_settings: ApplicationSettings, gdf: GeoDataFrame):
     assert gdf.geometry.apply(lambda geom: isinstance(geom, LineString)).all(), \
         ("De opgegeven vakindeling heeft niet voor elk vak een geometry. De "
          "applicatie sluit nu af.")
+
+    # Continue questioner
     specify_column_with_vaknaam(app_settings, gdf=gdf)
 
 
@@ -298,6 +299,12 @@ def align_vak_shp_to_dijktraject(
         gdf_new_vakindeling['id'] = gdf_new_vakindeling.index + 1
     gdf_new_vakindeling: GeoDataFrame = gdf_new_vakindeling[
         ["id", "naam", "m_start", "m_end", "geometry"]]
+
+    # Validate correct geometry types
+    valid, invalid_types = validate_geometry_types(gdf=gdf_new_vakindeling, allowed_types={"LineString"})
+    assert valid, (f"Most probably a bug. Invalid geometry types resulted from projecting the imported vakindeling "
+                   f"to the dijktraject-geometry. The invalid geometry types are {invalid_types}. Only LineStrings "
+                   f"were expected. Re-examine your input, or contact a developer with this error.")
 
     # Add to geopackage
     gdf_new_vakindeling.to_file(
