855ad46a3a83a4b1690339cdd066b49b69087d8c
Vincent Jilesen <v.jilesen@wshd.nl>
2026-06-11 10:42:43 +0200

    validator voor de expanded-table

--- tests/cmd_app/parameter_input/test_expand_input_tables.py
+++ tests/cmd_app/parameter_input/test_expand_input_tables.py
@@ -1,12 +1,9 @@
-# TODO fix parameter zowel per vak in excel en per utp in gis
-# TODO fix gis parameters niet uit expanded gevonden?
 """
 Voor idere rij in de excel en gis moet worden gecheckt of deze op de juiste
-plaats in de expanded staat. En of er iets is overgeslagen door dubble input.
+plaats in de expanded staat. En of er geen hogere prioriteit aanwezig is.
 traject->vak->utp
 scenario->wildcard
 """
-
 
 import ast
 import sqlite3
@@ -18,189 +15,217 @@ import pandas as pd
 gpkg_filepath = r"C:\Users\vinji\Python\Libraries\GEOprob-Pipe\Workspace\fix.geoprob_pipe.gpkg"
 
 
-def _extract_mean(val):
-    if pd.isna(val):
-        return np.nan
-    try:
-        d = ast.literal_eval(val)  # string → dict
+def get_mean(x):
+    if isinstance(x, dict):
+        return x.get("mean")
+    elif isinstance(x, str):
+        d = ast.literal_eval(x)  # string → dict
         return d.get("mean", np.nan)
-    except Exception:
-        return np.nan
+    return None
 
 
-def _vak(
-    df_expanded: pd.DataFrame,
-    df_excel_vak: pd.DataFrame,
-    utp_list: list,
-    parameter: str,
-):
-    mask = df_expanded["uittredepunt_id"].isin(utp_list)
+def _check_match(df_check: pd.DataFrame, df_compare: pd.DataFrame, key: str):
+    rel_tol = 1e-4
+    col = key.replace("mean", "match")
+    mask_nan = df_compare[key].isna()
 
-    df_expanded_vak = df_expanded.loc[~mask]
-    df_expanded_vak = df_expanded_vak.rename(
-        columns={"parameter_name": "parameter"}
-    )
-    df_expanded_vak["mean_val"] = df_expanded_vak["parameter_input"].apply(
-        _extract_mean
-    )
-    df_excel_vak = df_excel_vak.rename(
-        columns={"mean": "mean_val", "scope_referentie": "vak_id"}
-    )
-    # merge
-    keys = ["parameter", "vak_id", "ondergrondscenario_naam"]
-    df_compare = df_excel_vak.merge(
-        df_expanded_vak[keys + ["mean_val"]],
-        on=keys,
-        how="outer",
-        suffixes=("_excel", "_expanded"),
-        indicator=True,
+    df_check[col] = np.isclose(
+        df_compare["mean"], df_compare[key], rtol=rel_tol
     )
 
-    tolerance = 1e-4
+    df_check[col] = df_check[col].mask(mask_nan, None)
 
-    df_compare["verschil"] = np.abs(
-        df_compare["mean_val_excel"] - df_compare["mean_val_expanded"]
-    )
-    df_errors = df_compare[
-        (df_compare["_merge"] != "both")  # ontbrekende rijen
-        | (df_compare["verschil"] > tolerance)
-        | (
-            df_compare["mean_val_excel"].isna()
-            != df_compare["mean_val_expanded"].isna()
-        )
-    ].dropna(subset=["distribution_type"])
-    if len(df_errors) != 0:
-        print(f"Errors over vak bij {parameter}: {len(df_errors)}")
-        print(
-            df_errors[
-                [
-                    "parameter",
-                    "vak_id",
-                    "ondergrondscenario_naam",
-                    "mean_val_excel",
-                    "mean_val_expanded",
-                    "verschil",
-                ]
-            ].head()
-        )
-
-    return df_errors
+    return df_check
 
 
-def _utp(
-    df_expanded: pd.DataFrame,
-    df_excel_utp: pd.DataFrame,
-    utp_list: list,
-    parameter: str,
-):
-    mask = df_expanded["uittredepunt_id"].isin(utp_list)
-
-    df_expanded_utp = df_expanded.loc[mask]
-    df_expanded_utp = df_expanded_utp.rename(
-        columns={"parameter_name": "parameter"}
-    )
-    df_expanded_utp["mean_val"] = df_expanded_utp["parameter_input"].apply(
-        _extract_mean
-    )
-    df_excel_utp = df_excel_utp.rename(
-        columns={"mean": "mean_val", "scope_referentie": "uittredepunt_id"}
-    )
-    # merge
-    keys = ["parameter", "uittredepunt_id", "ondergrondscenario_naam"]
-
-    df_compare = df_excel_utp.merge(
-        df_expanded_utp[keys + ["mean_val"]],
-        on=keys,
-        how="outer",
-        suffixes=("_excel", "_expanded"),
-        indicator=True,
-    )
-
-    tolerance = 1e-4
-
-    df_compare["verschil"] = np.abs(
-        df_compare["mean_val_excel"] - df_compare["mean_val_expanded"]
-    )
-    df_errors = df_compare[
-        (df_compare["_merge"] != "both")  # ontbrekende rijen
-        | (df_compare["verschil"] > tolerance)
-        | (
-            df_compare["mean_val_excel"].isna()
-            != df_compare["mean_val_expanded"].isna()
-        )
-    ].dropna(subset=["distribution_type"])
-    if len(df_errors) != 0:
-        print(f"Errors over utp bij {parameter}: {len(df_errors)}")
-        print(
-            df_errors[
-                [
-                    "parameter",
-                    "uittredepunt_id",
-                    "ondergrondscenario_naam",
-                    "mean_val_excel",
-                    "mean_val_expanded",
-                    "verschil",
-                ]
-            ].head()
-        )
-
-    return df_errors
-
-
-def test_expand():
+def validate_expand_tables():
+    # --- Verzamel data op uit database
     conn = sqlite3.connect(gpkg_filepath)
+    df_out = pd.read_sql("SELECT * FROM data__expanded_parameters", conn)
+    df_excel = pd.read_sql("SELECT * FROM data__excel_parameter_invoer", conn)
+    df_excel["scope_referentie"] = df_excel["scope_referentie"].astype("Int64")
+    df_gis = pd.read_sql("SELECT * FROM data__gis_parameter_invoer", conn)
+    df_gis["scope_referentie"] = df_gis["scope_referentie"].astype("Int64")
+    df_gis["mean"] = df_gis["mean"].astype(float)
+    conn.close()
 
-    # Verzamel data
-    df_excel_vak = pd.read_sql(
-        "SELECT * FROM data__excel_parameter_invoer WHERE scope = 'vak'", conn
+    df_gis["scope"] = "gis_uittredepunt"
+    # Verwijder lege kolommen
+    df_excel = df_excel.dropna(axis=1, how="all")
+    df_gis = df_gis.dropna(axis=1, how="all")
+    df_input = pd.concat([df_excel, df_gis], ignore_index=True)
+
+    # --- Clean dataframes
+    df_input["ondergrondscenario_naam"] = df_input[
+        "ondergrondscenario_naam"
+    ].replace("", pd.NA)
+    df_out["ondergrondscenario_naam"] = df_out[
+        "ondergrondscenario_naam"
+    ].replace("", pd.NA)
+    df_out["uittredepunt_id"] = df_out["uittredepunt_id"].astype("Int64")
+    df_out = df_out.rename(columns={"parameter_name": "parameter"})
+
+    # Filter Dataframes
+    df_out["mean"] = df_out["parameter_input"].apply(get_mean)
+    df_out = df_out[
+        [
+            "parameter",
+            "vak_id",
+            "uittredepunt_id",
+            "ondergrondscenario_naam",
+            "mean",
+        ]
+    ]
+    df_input = df_input[
+        [
+            "parameter",
+            "scope",
+            "scope_referentie",
+            "ondergrondscenario_naam",
+            "mean",
+        ]
+    ]
+
+    # --- Merge per stap
+    df_compare = df_out.copy()
+
+    # Traject
+    df_traject = df_input.loc[df_input["scope"] == "traject"]
+    df_compare = df_compare.merge(
+        df_traject[["parameter", "mean"]],
+        on=["parameter"],
+        how="left",
+        suffixes=["", "_traject"],
     )
-    df_excel_utp = pd.read_sql(
-        "SELECT * FROM data__excel_parameter_invoer WHERE scope = 'uittredepunt'",
-        conn,
+
+    # Vak zonder scenario (Wildcard)
+    df_vak_wild = df_input.loc[
+        (df_input["scope"] == "vak")
+        & (df_input["ondergrondscenario_naam"].isna())
+    ]
+    df_vak_wild = df_vak_wild.rename(columns={"scope_referentie": "vak_id"})
+    df_compare = df_compare.merge(
+        df_vak_wild[["parameter", "vak_id", "mean"]],
+        on=["parameter", "vak_id"],
+        how="left",
+        suffixes=["", "_vak_wild"],
     )
-    df_gis_utp = pd.read_sql(
-        "SELECT * FROM data__gis_parameter_invoer WHERE scope = 'uittredepunt'",
-        conn,
+
+    # Vak met scenario
+    df_vak_scen = df_input.loc[
+        (df_input["scope"] == "vak")
+        & (df_input["ondergrondscenario_naam"].notna())
+    ]
+    df_vak_scen = df_vak_scen.rename(columns={"scope_referentie": "vak_id"})
+    df_compare = df_compare.merge(
+        df_vak_scen[
+            ["parameter", "vak_id", "ondergrondscenario_naam", "mean"]
+        ],
+        on=["parameter", "vak_id", "ondergrondscenario_naam"],
+        how="left",
+        suffixes=["", "_vak_scen"],
     )
-    if not df_excel_utp.empty:
-        df_utp = pd.concat(
-            [df_excel_utp, df_gis_utp], join="outer", ignore_index=True
-        )
-    else:
-        df_utp = df_gis_utp
 
-    df_expanded = pd.read_sql("SELECT * FROM data__expanded_parameters", conn)
+    # gis_utp zonder scenario (Wildcard)
+    df_gis_utp_wild = df_input.loc[
+        (df_input["scope"] == "gis_uittredepunt")
+        & (df_input["ondergrondscenario_naam"].isna())
+    ]
+    df_gis_utp_wild = df_gis_utp_wild.rename(
+        columns={"scope_referentie": "uittredepunt_id"}
+    )
+    df_compare = df_compare.merge(
+        df_gis_utp_wild[["parameter", "uittredepunt_id", "mean"]],
+        on=["parameter", "uittredepunt_id"],
+        how="left",
+        suffixes=["", "_gis_utp_wild"],
+    )
 
-    df_errors = pd.DataFrame()
-    for param in df_expanded["parameter_name"].unique().tolist():
-        mask_utp = df_utp["parameter"] == param
-        mask_vak = df_excel_vak["parameter"] == param
-        mask_expanded = df_expanded["parameter_name"] == param
-        utp_list = df_utp.loc[mask_utp, "scope_referentie"].unique().tolist()  # type:ignore
+    # excel_upt zonder scenario (Wildcard)
+    df_excel_utp_wild = df_input.loc[
+        (df_input["scope"] == "uittredepunt")
+        & (df_input["ondergrondscenario_naam"].isna())
+    ]
+    df_excel_utp_wild = df_excel_utp_wild.rename(
+        columns={"scope_referentie": "uittredepunt_id"}
+    )
+    df_compare = df_compare.merge(
+        df_excel_utp_wild[["parameter", "uittredepunt_id", "mean"]],
+        on=["parameter", "uittredepunt_id"],
+        how="left",
+        suffixes=["", "_excel_utp_wild"],
+    )
+    # gis_utp met scenario
+    df_gis_utp_scen = df_input.loc[
+        (df_input["scope"] == "gis_uittredepunt")
+        & (df_input["ondergrondscenario_naam"].notna())
+    ]
+    df_gis_utp_scen = df_gis_utp_scen.rename(
+        columns={"scope_referentie": "uittredepunt_id"}
+    )
+    df_compare = df_compare.merge(
+        df_gis_utp_scen[
+            ["parameter", "uittredepunt_id", "ondergrondscenario_naam", "mean"]
+        ],
+        on=["parameter", "uittredepunt_id", "ondergrondscenario_naam"],
+        how="left",
+        suffixes=["", "_gis_utp_scen"],
+    )
+    # excel_utp met scenario
+    df_excel_utp_scen = df_input.loc[
+        (df_input["scope"] == "uittredepunt")
+        & (df_input["ondergrondscenario_naam"].notna())
+    ]
+    df_excel_utp_scen = df_excel_utp_wild.rename(
+        columns={"scope_referentie": "uittredepunt_id"}
+    )
+    df_compare = df_compare.merge(
+        df_excel_utp_scen[["parameter", "uittredepunt_id", "mean"]],
+        on=["parameter", "uittredepunt_id"],
+        how="left",
+        suffixes=["", "_excel_utp_scen"],
+    )
 
-        df_errors = pd.concat(
-            [
-                df_errors,
-                _vak(
-                    df_expanded=df_expanded[mask_expanded],
-                    df_excel_vak=df_excel_vak[mask_vak],
-                    utp_list=utp_list,
-                    parameter=param,
-                ),
-            ]
-        )
-        df_errors = pd.concat(
-            [
-                df_errors,
-                _utp(
-                    df_expanded=df_expanded[mask_expanded],
-                    df_excel_utp=df_utp[mask_utp],
-                    utp_list=utp_list,
-                    parameter=param,
-                ),
-            ]
-        )
-    assert len(df_errors) == 0
+    # --- Check of waardes overeenkomen
+
+    df_check = df_out.copy()
+
+    df_check = _check_match(df_check, df_compare, "mean_traject")
+    df_check = _check_match(df_check, df_compare, "mean_vak_wild")
+    df_check = _check_match(df_check, df_compare, "mean_vak_scen")
+    df_check = _check_match(df_check, df_compare, "mean_gis_utp_wild")
+    df_check = _check_match(df_check, df_compare, "mean_excel_utp_wild")
+    df_check = _check_match(df_check, df_compare, "mean_gis_utp_scen")
+    df_check = _check_match(df_check, df_compare, "mean_excel_utp_scen")
+
+    df_errors = df_out.copy()
+    cols = [
+        "match_traject",
+        "match_vak_wild",
+        "match_vak_scen",
+        "match_gis_utp_wild",
+        "match_excel_utp_wild",
+        "match_gis_utp_scen",
+        "match_excel_utp_scen",
+    ]
+    df_errors["match"] = df_check[cols].any(axis=1)
+    
+    # Hoogste prioriteit check
+    df_errors["highest_priority"] = (
+        df_check[cols].idxmax(axis=1).where(df_errors["match"], None)
+    )
+   
+    arr = df_check[cols].to_numpy(dtype=object)
+
+    df_errors["priority_error"] = [
+        any(v is False for v in row[next((i for i, x in enumerate(row) if x is True), len(row))+1:])
+        for row in arr
+    ]
+
+    # Result
+    print(df_errors.loc[
+        (~df_errors["match"]) | (df_errors["priority_error"])
+    ])
 
 
-test_expand()
+validate_expand_tables()
