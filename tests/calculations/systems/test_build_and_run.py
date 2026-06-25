2a1b044321af4b0d68087db9fd471c59c7f89ea4
chrispijo <chrispitzalis@gmail.com>
2026-06-25 16:03:29 +0200

    start unit test update

--- tests/calculations/systems/test_build_and_run.py
+++ tests/calculations/systems/test_build_and_run.py
@@ -9,7 +9,8 @@ def test_worker():
 
     app_settings = ApplicationSettings()
     repo_root = repository_root_path()
-    filepath = os.path.join(repo_root, "tests", "systeem_testen", "224", "Traject224_MORIA_WBN_prob.geoprob_pipe.gpkg")
+    filepath = os.path.join(repo_root, "tests", "systeem_testen", "224", "unit_testset_dt224.geoprob_pipe.gpkg")
+    assert os.path.exists(filepath)
     app_settings.workspace_dir = os.path.dirname(filepath)
     app_settings.geopackage_filename = os.path.basename(filepath)
     model = app_settings.geohydrologisch_model
@@ -17,7 +18,7 @@ def test_worker():
         geohydrologisch_model=model,
         geopackage_filepath=filepath,
         to_run_vakken_ids=None)
-    result = _worker(row_unique={'uittredepunt_id': 1, 'ondergrondscenario_naam': 'scenario1', 'vak_id': 4})
+    _ = _worker(row_unique={'uittredepunt_id': 1, 'ondergrondscenario_naam': 'scenario1', 'vak_id': 4})
 
     ##
 
@@ -32,7 +33,8 @@ def test_build_and_run_system_calculations():
 
     app_settings = ApplicationSettings()
     repo_root = repository_root_path()
-    filepath = os.path.join(repo_root, "tests", "systeem_testen", "224", "Traject224_MORIA_WBN_prob.geoprob_pipe.gpkg")
+    filepath = os.path.join(repo_root, "tests", "systeem_testen", "224", "unit_testset_dt224.geoprob_pipe.gpkg")
+    assert os.path.exists(filepath)
     app_settings.workspace_dir = os.path.dirname(filepath)
     app_settings.geopackage_filename = os.path.basename(filepath)
     app_settings.to_run = "vakken:4,5"
