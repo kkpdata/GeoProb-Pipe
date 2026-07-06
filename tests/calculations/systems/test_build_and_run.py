5840e3b4455a4859464642ffe9a3b7bc832cab1e
chrispijo <chrispitzalis@gmail.com>
2026-07-06 11:12:39 +0200

    DF test

--- tests/calculations/systems/test_build_and_run.py
+++ tests/calculations/systems/test_build_and_run.py
@@ -23,6 +23,33 @@ def test_worker():
     ##
 
 
+def test_collect_df_and_worker():
+
+    ##
+
+    from geoprob_pipe import SystemCalculation
+    from geoprob_pipe.results.construct_dataframes import collect_df_beta_scenario_final
+    from geoprob_pipe.calculations.systems.mappers.calculations import CALCULATION_MAPPER
+    import os
+    from repo_utils.utils import repository_root_path
+    from geoprob_pipe.calculations.systems.build_and_run import _worker
+
+    repo_root = repository_root_path()
+    geopackage_filepath: str = os.path.join(
+        repo_root, "tests", "systeem_testen", "224", "unit_testset_dt224.geoprob_pipe.gpkg")
+    builder = CALCULATION_MAPPER["model4a"]["system_builder"](
+        geopackage_filepath=geopackage_filepath, to_run_vakken_ids=None)
+    row_unique = {'uittredepunt_id': 1, 'ondergrondscenario_naam': 'PL', 'vak_id': 0}
+    calc: SystemCalculation = builder.build_instance(row_unique=row_unique)
+    calc.run()
+    _ = collect_df_beta_scenario_final(calc)
+
+    # Worker
+    _ = _worker(row_unique)
+
+    ##
+
+
 def test_build_and_run_system_calculations():
     ##
 
